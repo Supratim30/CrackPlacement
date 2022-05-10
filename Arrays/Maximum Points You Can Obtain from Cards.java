5da1ded1e9c2b74c05ef4dc3fdaf7edb0d93bfb2
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-05-10 19:48:37 +0530

    Update Maximum Points You Can Obtain from Cards.java

--- Arrays/Maximum Points You Can Obtain from Cards.java
+++ Arrays/Maximum Points You Can Obtain from Cards.java
@@ -1,1 +1,23 @@
+//leetcode 1423
 
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int windowSize = n-k;
+        int windowValue = 0;
+        int total = 0;
+        
+        for(int i : cardPoints) total+=i;
+        
+        for(int i = 0;i<windowSize;i++) windowValue+=cardPoints[i];
+        
+        int min = windowValue;
+        
+        for(int i = windowSize;i<n;i++){
+            windowValue= windowValue+cardPoints[i]-cardPoints[i-windowSize];
+            min = Math.min(min,windowValue);
+        }
+        
+        return total-min;
+    }
+}
