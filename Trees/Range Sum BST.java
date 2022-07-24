dbd6d97897d4cf23b486e02bb2f507a961179524
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-07-24 22:27:50 +0530

    Update Range Sum BST.java

--- Trees/Range Sum BST.java
+++ Trees/Range Sum BST.java
@@ -37,3 +37,21 @@ class Solution {
     }
     
 }
+
+
+//RECURSIVE
+
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+            int sum = 0;
+
+        if(root == null) return 0;
+        if(root.val >= low && root.val <= high){
+            sum+= root.val;
+        }
+        if(root.val < high) sum += rangeSumBST(root.right, low, high);
+        if(root.val > low) sum += rangeSumBST(root.left, low, high);
+        
+        return sum;
+    }
+}
