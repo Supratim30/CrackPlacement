ade762e910c542943fe95a6eb3bd54c77d91d86e
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-08-31 00:04:30 +0530

    Time: 2 ms (76.18%), Space: 41.4 MB (52.54%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -0,0 +1,9 @@
+class Solution {
+
+    public int divide(int dividend, int divisor) {
+        if (dividend < 0 && divisor < 0) {
+            if (dividend == Integer.MIN_VALUE && (divisor == 1 || divisor == -1)) return Integer.MAX_VALUE;
+        }
+        return dividend / divisor;
+    }
+}
