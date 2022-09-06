4a4599e28be8972aa3d3a574cff326ba43d3c32f
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-09-06 19:00:37 +0530

    Time: 1 ms (97.70%), Space: 44.6 MB (73.01%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+//kadane's algo
+    public int maxProduct(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        int prod = 1;
+        for (int i = 0; i < nums.length; i++) {
+            prod *= nums[i];
+            res = Math.max(res, prod);
+            if (prod == 0) prod = 1;
+        }
+        
+        prod = 1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            prod *= nums[i];
+            res = Math.max(res, prod);
+            if (prod == 0) prod = 1;
+        }
+        return res;
+    }
+}
