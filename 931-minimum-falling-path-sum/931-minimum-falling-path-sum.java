7c7c788b1f151ee1590c1e2a3391904cffa4f65b
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-09-10 14:47:41 +0530

    Time: 10 ms (15.99%), Space: 47.7 MB (46.20%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,32 +1,38 @@
 class Solution {
-
     public int minFallingPathSum(int[][] matrix) {
-        //top down
-        int n = matrix.length;
-        int m = matrix[0].length;
 
-        int[][] dp = new int[n][m];
-        for (int[] arr : dp) Arrays.fill(arr, -1);
-        int mini = Integer.MAX_VALUE;
+    int min = solution(matrix, matrix.length - 1, matrix[0].length - 1);
+    return min;
+}
 
-        for (int j = 0; j < m; j++) {
-            int ans = helper(n - 1, j, m, dp, matrix);
-            mini = Math.min(mini, ans);
+public static int solution(int[][] board, int lastrow, int lastcol){
+
+    
+    int dp[][] = new int[lastrow+1][lastcol+1];
+    
+    //tabulation
+    for(int col = 0; col <= lastcol; col++){
+        dp[lastrow][col] = board[lastrow][col];
+    }
+    
+    for(int i = lastrow - 1; i >= 0; i--){
+        for(int j = lastcol; j >= 0; j--){
+            if(j == lastcol){
+                dp[i][j] = Math.min(dp[i+1][j-1],dp[i+1][j]) + board[i][j];
+            } else if(j == 0){
+                dp[i][j] = Math.min(dp[i+1][j+1],dp[i+1][j]) + board[i][j];    
+            } else {
+                dp[i][j] = Math.min(dp[i+1][j+1],(Math.min(dp[i+1][j-1],dp[i+1][j]))) + board[i][j];
+            }
         }
-        return mini;
     }
-
-    private int helper(int i, int j, int m, int[][] dp, int[][] matrix) {
-//base conditions
-        if (j < 0 || j >= m) return (int) Math.pow(10, 9);
-        if (i == 0) return matrix[0][j];
-        
-        
-        if (dp[i][j] != -1) return dp[i][j];
-        int up = matrix[i][j] + helper(i - 1, j, m, dp, matrix);
-        int leftD = matrix[i][j] + helper(i - 1, j - 1, m, dp, matrix);
-        int rightD = matrix[i][j] + helper(i - 1, j + 1, m, dp, matrix);
-
-        return dp[i][j] = Math.min(up, Math.min(leftD, rightD));
+    
+    int min = Integer.MAX_VALUE;
+    for(int j = 0; j <= lastcol; j++){
+        if(dp[0][j] < min){
+            min = dp[0][j];
+        }
     }
+    return min;
+}
 }
