8cc588fbcd1f6ab8d6371f540384702ed0bf1042
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-09-15 19:38:36 +0530

    Time: 105 ms (82.04%), Space: 61.1 MB (91.02%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        int n = changed.length;
         Arrays.sort(changed);
-        
-        int[] res = new int[n / 2];
-        Queue<Integer> q = new LinkedList<>();
-        int i = 0;
-        
-        for(int nums : changed){
-            if(!q.isEmpty() && nums == q.peek()) q.poll();
-            else{
-                if(i == res.length) return new int[0];
-                q.offer(nums * 2);
-                res[i++] = nums;
-            }
+        int n = changed.length, original[]=new int[n/2],i=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num: changed){
+            if(num%2==0 && map.containsKey(num/2)){
+                remove(num/2,map);
+                original[i++]=num/2;
+            }else map.put(num,map.getOrDefault(num,0)+1);
         }
-        return q.isEmpty() ? res : new int[0];
+        return (n%2==0 && i==n/2)?original:new int[0];
+    }
+    void remove(int key, HashMap<Integer,Integer> map){
+        int freq=map.get(key);
+        if(freq==1) map.remove(key);
+        else map.put(key,freq-1);
     }
 }
