8627cedd6d73d6707e21ca2c20e5b1bc2c138258
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-09-24 13:56:07 +0530

    Time: 10 ms (78.94%), Space: 43.8 MB (42.76%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,22 +1,31 @@
 class Solution {
-
-    public int findTargetSumWays(int[] nums, int target) {
-        int ans = allsets(nums, 0, 0, target, 0);
-        return ans;
+public int findTargetSumWays(int[] nums, int target) {
+     int n=nums.length;
+    int sum=0;
+    
+    for(int num : nums) sum += num;
+    
+   if((sum-target)%2==1 || (target>sum)) return 0;
+    
+    sum=(sum+target)/2;
+    if(sum<0) return 0;
+    return getCount(nums,sum,nums.length);
+}
+public int getCount(int[] nums,int target,int size){
+    int[][] arr = new int[size+1][target+1];
+    for(int i=0;i<size+1;i++){
+        for(int j=0;j<target+1;j++){
+        if(i==0) arr[i][j]=0;
+        if(j==0) arr[i][j]=1;
+    } 
     }
-
-    public static int allsets(int[] nums, int idx, int sum, int target, int count) {
-        if (idx == nums.length) {
-            if (sum == target) {
-                count++;
-            }
-            return count;
-        }
-
-        int ansp = allsets(nums, idx + 1, sum + nums[idx], target, count);
-
-        int ansm = allsets(nums, idx + 1, sum - nums[idx], target, count);
-
-        return ansp + ansm;
+    
+    for(int i=1;i<size+1;i++){
+        for(int j=0;j<target+1;j++){
+        if(nums[i-1]>j) arr[i][j]=arr[i-1][j];
+        else arr[i][j]=arr[i-1][j - nums[i-1]]+arr[i-1][j];
+    } 
     }
+    return arr[size][target];
+}
 }
