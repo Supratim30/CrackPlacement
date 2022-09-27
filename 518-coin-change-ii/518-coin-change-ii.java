2a940741c4a6a5837c2b9bcc86a8287c477b622e
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2022-09-27 07:24:34 +0530

    Time: 23 ms (21.35%), Space: 51.3 MB (21.09%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -1,19 +1,21 @@
 class Solution {
 
+    //tabulation
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
-        for (int[] a : dp) Arrays.fill(a, -1);
-        return helper(n - 1, amount, coins, dp);
-    }
-//funda is not to update the idx(for take) as there are infinite supply of coins
-    private int helper(int idx, int target, int[] coins, int[][] dp) {
-        if (idx == 0) return target % coins[0] == 0 ? 1 : 0;
-        if (dp[idx][target] != -1) return dp[idx][target];
-        int notTake = helper(idx - 1, target, coins, dp);
-        int take = 0;
-        if (coins[idx] <= target) take = helper(idx, target - coins[idx], coins, dp);
+        for (int[] a : dp) Arrays.fill(a, 0);
 
-        return dp[idx][target] = notTake + take;
+        for (int t = 0; t <= amount; t++) dp[0][t] = t % coins[0] == 0 ? 1 : 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int tar = 0; tar <= amount; tar++) {
+                int notTake = dp[i - 1][tar];
+                int take = 0;
+                if (coins[i] <= tar) take = dp[i][tar - coins[i]];
+                dp[i][tar] = take + notTake;
+            }
+        }
+        return dp[n - 1][amount];
     }
 }
