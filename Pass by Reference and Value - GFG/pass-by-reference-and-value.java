ee1bd9ad31fd43cddd44d99da384b455117d7e58
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2023-06-13 14:54:07 +0530

    Added solution - LeetHub

--- Pass by Reference and Value - GFG/pass-by-reference-and-value.java
+++ Pass by Reference and Value - GFG/pass-by-reference-and-value.java
@@ -29,10 +29,7 @@ class GFG {
 class Solution {
     static int[] passedBy(int a, int b) {
         // code here
-        int[] arr = new int[2];
-        arr[0] = a + 1;
-        arr[1] = b + 2;
-        
+        int arr[] = {a+1,b+2};
         return arr;
     }
 }
