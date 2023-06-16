993562705219509e5f70f60a9d083b875a5dd35c
Supratim Majumder <49454292+Supratim30@users.noreply.github.com>
2023-06-17 00:04:57 +0530

    Added solution - LeetHub

---
+++ Pattern 10 - GFG/pattern-10.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int N) {
+        // code here
+        for(int i=1;i<=2*N-1;i++){
+        
+          // stars would be equal to the row no. uptill first half
+          int stars = i;
+          
+          // for the second half of the rotated triangle.
+          if(i>N) stars = 2*N-i;
+          
+          // for printing the stars in each row.
+          for(int j=1;j<=stars;j++){
+              System.out.print("* ");
+          }
+          
+          // As soon as the stars for each iteration are printed, we move to the
+          // next row and give a line break otherwise all stars
+          // would get printed in 1 line.
+          System.out.println();
+      }
+    }
+}
